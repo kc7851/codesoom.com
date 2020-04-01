9bcfdc74f2a0cceaeec0b6ab559cc78b2633f40e
wholemann <wholeman.dev@gmail.com>
2020-04-01 17:47:05 +0900

    Add Details component
    Add Details component for describing details of our service.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from './components/Header';
 import Description from './components/Description';
 import Features from './components/Features';
+import Details from './components/Details';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Description />
       <Features />
+      <Details />
     </>
   );
 }
