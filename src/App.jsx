f7804faef97a86964a50bae137bac21d37c63f56
wholemann <wholeman.dev@gmail.com>
2020-04-01 17:22:49 +0900

    Add Features component
    Add Features component for describing key features of our service.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from './components/Header';
 import Description from './components/Description';
+import Features from './components/Features';
 
 function App() {
   return (
     <>
       <Header />
       <Description />
+      <Features />
     </>
   );
 }
