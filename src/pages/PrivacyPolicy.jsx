988eef8221e4bef7a2ec6b06d06aceebddfbd050
hannut91 <hannut91@gmail.com>
2020-04-08 15:05:03 +0900

    Remove duplicated class
    Remove duplicated class name with `cls-n` that fill color of shape.

---
+++ src/pages/PrivacyPolicy.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function PrivacyPolicy() {
+  return (
+    <>
+      {/* <PrivacyPolicyHeader /> */}
+      <header className="ex-header">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1>Privacy Policy</h1>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
