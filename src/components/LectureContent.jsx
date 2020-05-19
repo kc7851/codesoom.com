ea5eba72314055c10e3e9d7a249999762812764d
hannut91 <hannut91@gmail.com>
2020-05-19 18:11:30 +0900

    Refactor style
    Refactor style for better design.

--- src/components/LectureContent.jsx
+++ src/components/LectureContent.jsx
@@ -2,17 +2,32 @@ import React from 'react';
 
 import { useSelector } from 'react-redux';
 
+import ReactMarkdown from 'react-markdown';
+
+import facepaint from 'facepaint';
+
 import { drawerWidth } from '../assets/styles/common';
 
-import mq from '../styles/breakpoints';
+// TODO: Replace real data in production
+import ReactIntro from '../assets/documents/intro-react.md';
+
+const breakpoints = [576, 1348];
+
+const mq = facepaint(
+  breakpoints.map((bp) => `@media (min-width: ${bp}px)`),
+);
 
 const styles = {
   opendContent: mq({
-    marginLeft: [0, drawerWidth],
+    paddingLeft: ['16px', `calc(${drawerWidth} + 16px)`, '16px'],
+    maxWidth: ['initial', 'initial', '868px'],
+    margin: ['initial', 'initial', 'auto'],
   }),
   content: {
     padding: '62px 16px 12px 16px',
     width: '100%',
+    margin: 'auto',
+    maxWidth: '868px',
   },
 };
 
@@ -22,11 +37,11 @@ export default function LectureContent() {
   return (
     <main
       css={[
-        drawerOpen && styles.opendContent,
         styles.content,
+        drawerOpen && styles.opendContent,
       ]}
     >
-      메인
+      <ReactMarkdown escapeHtml={false} source={ReactIntro} />
     </main>
   );
 }
